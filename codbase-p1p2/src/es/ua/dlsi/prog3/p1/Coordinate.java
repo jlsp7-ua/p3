f99cb6b61e4abb91f68aeea9a6cfc952d0c39857
jose luis <jlsp7@alu.ua.es>
2024-09-30 13:25:03 +0200

    Práctica 1 terminada. Pasa los controles de documentación y código.

--- codbase-p1p2/src/es/ua/dlsi/prog3/p1/Coordinate.java
+++ codbase-p1p2/src/es/ua/dlsi/prog3/p1/Coordinate.java
@@ -1,76 +1,95 @@
 package es.ua.dlsi.prog3.p1;
-/**
-    @author José Luis Solivella Poveda
-    @version 1.0
-    DNI: 29521020 Z
-*/
-
 import java.util.Arrays;
 
 /**
-    La clase es inmutable porque se ha declarado final, no tiene setters, los atributos son privados y definidos con final, no se referencia a ningún objeto externo mutable ni se ofrece una referencia que pueda modificar el contendio a través de los getters.
-    */
+ * Esta clase represenata una cordenada de n dimensiones.
+ * La clase es inmutable porque se ha declarado final, no tiene setters,
+ * los atributos son privados y definidos con final, no se referencia a ningún objeto
+ * externo mutable ni se ofrece una referencia que pueda modificar el contendio
+ * a través de los getters.
+ * 
+ * @author José Luis Solivella Poveda 29521020Z
+*/
 public final class Coordinate {
 
     /**
-        Array atributo de la clase que contiene las coordenadas.
+     * Array atributo de la instancia que contiene las coordenadas.
+     * Cada objeto Coordinate tendrá sus propias componentes, por eso este atributo es de instancia y no de clase.
+     * Para garantizar que sea inmutable se ha establecido como final.
     */
     private final double[] components;
 
     /**
-        Constructor de la clase a partir de array de componentes.
-        @param componentes : paso por referencia del array de componentes, que si es null tendrá longitud 0
+    *   Constructor sobrecargado de la clase Coordinate.
+    *   Crea un objeto Coordinate a partir de un array de coordenadas del que se hace
+    *   una copia defensiva para evitar modificaciones desde el código cliente.
+    *   Si el array es null, se incializa con longitud 0.
+    *   
+    *   @param components : paso por referencia del array de componentes, que si es null tendrá longitud 0
     */
     public Coordinate(double[] components) {
-        if (components != null) {
-            this.components = components.clone(); // Para evitar referenciar a objetos mutables
-        } else {
-            this.components = new double[0];
-        }
+        if (components == null) this.components = new double[0];   // Si el parametro es una lista vacía crea una vacía 
+        else this.components = components.clone(); // Para evitar referenciar a objetos mutables
     }
 
     /**
-        Constructor de copia
-        @param coordinate : paso por referencia de un objeto Coordinate
+    *   Constructor de copia.
+    *   A partir de un objeto Coordinate crea otro con los mismos atributos.
+    *   Se utiliza copia defensiva en las componentes para evitar que el código cliente modifique el objeto original.
+    *   
+    *   @param coordinate : paso por referencia de un objeto Coordinate
     */
     public Coordinate(Coordinate coordinate) {
-        this.components = coordinate.components.clone(); // Copia defensiva para inmutabilidad
+        this.components = coordinate.components.clone(); // Copia defensiva de las componentes para inmutabilidad
     }
 
     /**
-        getter de las componentes
-        @return Devuelve la refencia a una copia del array de coordenadas del objeto.
+    *   Método getter de las componentes.
+    *   Método para obtener las componentes que conforman el array components.
+    *   Para evitar modificaciones en el objeto se devuelve una copia defensiva.
+    *   
+    *   @return Devuelve la refencia a una copia del array de coordenadas del objeto.
     */
     public double[] getComponents() {
-        return components.clone();
+        return components.clone();	// Devolvemos una copia de las componentes para asegurar la inmutabilidad
     }
 
     /**
-        Método get para conocer las dimensiones
-        @return Utiliza el método 'lenght' de los arrays para devolver el número de dimensiones de la coordenada.
+    *   Método get dimensiones.
+    *   Método que calcula en qué dimensión se encuentra la coordenada a partir del array components.
+    *   
+    *   @return Utiliza el método 'length' de los arrays para devolver el número de dimensiones de la coordenada.
     */
     public int getDimensions() {
-        return components.length;
+        return components.length;	// Devolvemos la longitud del array de componentes, que son las dimensiones
     }
-
     /**
-        Método para generar el hashCode
-        @return Deevuelve el hashCode de la coordenada gracias al método hashCodede los arrays.
+    *   Método hashCode.
+    *   Método que genera el hashcode asociado al array components.
+    *   @return Devuelve el hashCode de la coordenada gracias al método hashCode los arrays.
     */
-    public int hashCode() {
-        return Arrays.hashCode(components);
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;	// Establecemos un número primo
+		int result = 1;	// Establecemos un resultado
+		result = prime * result + Arrays.hashCode(components);	// Calculamos el resultado
+		return result;	// Devolvemos el resultado
+	}
 
-    /**
-        Método de comparación
-        Compara el estado de dos objetos Coordinate.
-        @param obj : objeto con el cual será comparado.
-        @return Devuelve verdadero si el estado de los objetos son iguales y falso si no es así o son objetos de distintas clases.
-    */
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Coordinate)) return false;
-        Coordinate objCoor = (Coordinate) obj;
-        return Arrays.equals(this.components, objCoor.components);
-    }
+	/**
+	 *	Método equals.
+	 *	Método que comprueba si un objeto obj es igual a uno Coordinate,`
+	 *	comprobando si la referencia es la misma, si las componentes son iguales
+	 *  y por último comparando los objetos directamente.
+	 *  
+	 *  @param obj que es el objeto a comparar con el objeto Coordinate.
+	 *	@return devuelve verdadero si son iguales y falso si no lo son.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;	// Si el objeto pasdo es una referencia al que estamos comparado devolvemos verdadero
+		if (!(obj instanceof Coordinate)) return false;	// Si no es un objeto de la clase cCoordinate no puede ser igual
+		Coordinate other = (Coordinate) obj;	// Asignamos el objeto al tipo Coordinate
+		return Arrays.equals(components, other.components);	// Los comparamos
+	}
 }
--- codbase-p1p2/src/es/ua/dlsi/prog3/p1/SummaryStatistics.java
+++ codbase-p1p2/src/es/ua/dlsi/prog3/p1/SummaryStatistics.java
@@ -1,49 +1,129 @@
 package es.ua.dlsi.prog3.p1;
 import java.util.ArrayList;
 
+/**
+ * Clase Summary Statisticas.
+ * Representa las estadísticas sobre un array de la calase Integer.
+ * Entre los cálculos se encuentran el máximo, mínimo y media. 
+ * Además, permite agregar nuevos valores al array y contar cuantos son.
+ * Se utiliza la copia defensiva para evitar que el arraylist orignila sea modificado por el código cliente.
+ * @author José Luis Solivella Poveda 29521020Z
+*/
 public class SummaryStatistics {
+	/**
+	 * Atributo de la clase que almacena el id que será asignado a la próxima instancia de la clase.
+	 * Todos los objetos pueden acceder a este atributo que tiene el mismo valor para todos al ser atributo de clase.
+	 */
     private static int NEXT_ID = 1;
+    /**
+     * Atributo de instancia que indica el id del objeto.
+     * Cada objeto tiene su propio id, por eso es un atributo de instancia.
+     */
     private int id;
+    /**
+     * Atributo de instancia que contiene el array de valores sobre los que se calculará la estadística.
+     * Es de instancia porque cada objeto contiene unos valores diferentes dentro del array.
+     */
     private ArrayList<Integer> values;
-
+    /**
+     * Constructor por defecto de la clase.
+     * Construye un objeto SummaryStatistics asignńdole un id y creando una lista vacía.
+     * No requiere de ningún parámetro.
+     */
     public SummaryStatistics() {
-        id = NEXT_ID++;
-        values = new ArrayList<>();
+        id = NEXT_ID++;	// Asignamos el id y aumentamos NEXT_ID
+        values = new ArrayList<>();	// Creamos un arraylist vacío
     }
+    /**
+     * Constructor sobrecargado de la clase conociendo el arraylist de valores inicial.
+     * Se usa cuando disponemos desde el primer momento de los valores sobre los que queremos obtener las estadísticas..
+     * Se usa copia defensiva en el arraylist de valores.
+     * 
+     * @param values ArrayList que contiene los enteros para crear el objeto
+     */
     public SummaryStatistics(ArrayList<Integer> values) {
-        id = NEXT_ID++;
-        this.values = new ArrayList<>(values); // Copia defensiva
+        id = NEXT_ID++;	// Asignamos el id y aumentamos NEXT_ID
+        this.values = new ArrayList<>(values); // Copia defensiva del arraylist de valores
     }
+    /**
+     * Constructor de copia.
+     * Construye un objeto SummaryStatistics a partir de la copia defensiva de otro.
+     * La copia defensiva o copia profunda evita que se modifique el objeto pasado por parámetro. 
+     * 
+     * @param ss Objeto SummaryStatistics
+     */
     public SummaryStatistics(SummaryStatistics ss) {
-        this.id = ss.id;
-        this.values = new ArrayList<>(ss.values);
-        NEXT_ID++;
+        this.id = NEXT_ID++;	// Asignamos el id y aumentamos NEXT_ID
+        this.values = new ArrayList<>(ss.values); // Copia defensiva del arraylist de valores del objeto pasado
     }
+    /**
+     * Método add.
+     * Método para añadir valores al array values para realizar la estadística.
+     * @param value entero que se añadirá al arraylist values
+     */
     public void add (int value) {
-        values.add(value);
+        values.add(value); // Añade un valor al arraylist de valores
     }
+    /**
+     * Método que getID.
+     * Método getter del atributo id del objeto.
+     * @return id del objeto actual (int)
+     */
     public int getId() {
-        return id;
+        return id; // retorna el valor del id
     }
+    /**
+     * Método que getAvarage.
+     * Método que calcula la media de los valores del array values.
+     * 
+     * @return Media de valores Integer.
+     */
     public Integer getAverage() {
-        if (values.isEmpty()) return null;
-        int suma = 0;
-        for (Integer num: values) {
-            suma += num.intValue();
+        if (values.isEmpty()) return null; // Si el array esta vacío, devolvemos null
+        int suma = 0;	// Establecemos la suma en 0
+        for (Integer num: values) {	// Para cada valor del array
+            suma += num.intValue();	// lo sumamos, haciendo unbocxhing del valor
         }
-        return suma/values.size();
+        return suma/values.size();	// Calculamos la media
     }
+    /**
+     * Método getMax.
+     * Método que iterando sobre el array de valores encuentra el valor máximo.
+     * Si el array está vacío devolverá null.
+     * Hace unboxing al usar Math::max.
+     * 
+     * @return Devuelve el Integer máximo.
+     */
     public Integer getMax() {
-        return values.isEmpty() ? null : values.stream().reduce(Integer.MIN_VALUE, Math::max);
-        // Tomamos `Integer.MIN_VALUE` para asegurarnos que coge el primer elemento de `values`
+        return values.isEmpty() ? null : values.stream().reduce(Integer.MIN_VALUE, Math::max); // Tomamos `Integer.MIN_VALUE` para asegurarnos que coge el primer elemento de `values`
     }
+    /**
+     * Método getMin.
+     * Método que iterando sobre el array de valores encuentra el valor mínimo.
+     * Si el array está vacío devolverá null.
+     * Hace unboxing al usar Math::min.
+     * 
+     * @return Devuelve el Integer mínmo.
+     */
     public Integer getMin() {
-        return values.isEmpty() ? null : values.stream().reduce(Integer.MAX_VALUE, Math::min);
+        return values.isEmpty() ? null : values.stream().reduce(Integer.MAX_VALUE, Math::min);	// Tomamos `Integer.MAX_VALUE` para asegurarnos que coge el primer elemento de `values`
     }
+    /**
+     * Método getSize.
+     * Método que calcula el tamaño del array de valores.
+     * 
+     * @return Devuevle un int con el tamaño del array.
+     */
     public int getSize() {
-        return values.size();
+        return values.size();	// Retronamos el valor del tamaño del array
     }
+    /**
+     * Método de la clase COUTN_INSTANCES
+     * Método que cuenta la cantidad de objetos de la clase SummaryTest creados.
+     * 
+     * @return int número de instancias.
+     */
     public static int COUNT_INSTANCES() {
-        return NEXT_ID-1;
+        return NEXT_ID-1;	// Devolvemos el numero de instancias restando 1 al siguiente id
     }
 }
